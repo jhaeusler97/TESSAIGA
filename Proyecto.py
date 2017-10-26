c480855d0d2ee74f43bf63a27ae8c741aaed2453
jose <haeuslerjose@gmail.com>
2017-10-25 23:26:26 -0500

    Base Login

---
+++ Proyecto.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, request, g, redirect, url_for, render_template, flash, session
+import flask
+import requests
+import sys
+from flask import json
+import math
+from sqlalchemy import *
+import os
+
+app = Flask(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route("/")
+def login():
+	titulo = "Home!"
+	return render_template("login.html")
+
+if __name__ == "__main__":
+	app.run(debug = True, port = 5000)
